70fe188b3b39e08a69a03ff717fb619bc9db283a
Joel Porquet <joel@porquet.org>
2015-01-01 18:23:53 +0100

    add a setup.py file for automatic installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = "etcinstall",
+        version = "0.1",
+        description = "Local etc installer",
+        url = "https://joel.porquet.org/wiki/hacking/etcinstall/",
+        author = "Joël Porquet",
+        author_email = "joel@porquet.org",
+        license = "GPL3",
+        scripts = ["etcinstall"],
+        )
